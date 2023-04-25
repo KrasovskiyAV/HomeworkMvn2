318f0a465d24534058dfd9acdf4a876b353fb7a7
Красовский АВ <AV.Krasovskiy@ya.ru>
2023-04-25 11:30:59 +0700

    csv added

--- src/test/java/ru/netology/services/HolidayServiceTest.java
+++ src/test/java/ru/netology/services/HolidayServiceTest.java
@@ -1,26 +1,17 @@
 package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class HolidayServiceTest {
 
-    @Test
-    public void shouldHolidayMonthExpected3() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/holiday.csv")
+    public void shouldHolidayMonthExpected3(int expected, int income, int expenses, int threshold) {
         HolidayService service = new HolidayService();
 
-        int expected = 3;
-        int actual = service.calculate(10_000, 3_000, 20_000);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldHolidayMonthExpected2() {
-        HolidayService service = new HolidayService();
-
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
